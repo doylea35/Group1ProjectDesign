e7d7593c4d4153e4d3c2d56078d496532c53518c
Aina <aina.gomila@estudiantat.upc.edu>
2025-02-21 15:05:44 +0000

    updated users and login

--- backend/api/routes/user.py
+++ backend/api/routes/user.py
@@ -1,8 +1,9 @@
 from fastapi import APIRouter, HTTPException, Query,  Depends, status, Body
 from typing import Dict, Optional
-from db.database import groups_collection, users_collection, tasks_collection
+from db.database import groups_collection, users_collection, tasks_collection, subteams_collection
 from db.models import User, Group, Task
 from db.schemas import users_serial, groups_serial, tasks_serial
+from api.request_model.user_request_schema import CreateUserRequest, DeleteUserRequest, UpdateUserRequest
 from bson import ObjectId # mongodb uses ObjectId to store _id
 import bcrypt
 import jwt
@@ -59,28 +60,36 @@ class UserRegister(BaseModel):
     name: str
     email: str
     password: str
+    groups: list[str]
 
 
 
 @user_router.post("/register")
-async def register_user(user_data : UserRegister):
-    existing_user = users_collection.find_one({"email": user_data.email})
+async def register_user(request : UserRegister):
+    existing_user = users_collection.find_one({"email": request.email})
     if existing_user:
         raise HTTPException(status_code=400, detail="Email already registered")
 
-    hashed_password =  hash_password(user_data.password)
+    hashed_password =  hash_password(request.password)
     confirmation_code = "123"
+
+    # Validate group IDs
+    valid_group_ids = []
+    for group_id in request.groups:
+        if not groups_collection.find_one({"_id": ObjectId(group_id)}):
+            raise HTTPException(status_code=400, detail=f"Group {group_id} does not exist")
+        valid_group_ids.append(ObjectId(group_id))
     
     new_user = {
-        "name": user_data.name,
-        "email": user_data.email,
+        "name": request.name,
+        "email": request.email,
         "password": hashed_password,
         "status": "Pending",
         "confirmation_code": confirmation_code
     }
 
     result = users_collection.insert_one(new_user)
-    unique_token = generate_token(str(result.inserted_id), user_data.email)
+    unique_token = generate_token(str(result.inserted_id), request.email)
     
     users_collection.update_one(
         {"_id": result.inserted_id},
@@ -90,17 +99,17 @@ async def register_user(user_data : UserRegister):
     confirmation_link = BASE_URL.format(confirmationCode=confirmation_code)
 
     message = REGISTRATION_CONFIRMATION_EMAIL_TEMPLATE.format(
-        user_name=user_data.name,
+        user_name=request.name,
         confirmation_link=confirmation_link
     )
 
     # Send confirmation email
-    email_sender.send_email(receipient=user_data.email, email_message=message, subject_line="Please confirm your email.")
+    email_sender.send_email(receipient=request.email, email_message=message, subject_line="Please confirm your email.")
 
     return {
         "id": str(result.inserted_id),
-        "name": user_data.name,
-        "email": user_data.email,
+        "name": request.name,
+        "email": request.email,
         "token": unique_token,
         "status": "Pending",
         "confirmation_code": unique_token,
@@ -140,6 +149,55 @@ async def login_user(form_data: OAuth2PasswordRequestForm = Depends()):
         "status": user["status"],
     }
 
+@user_router.delete("/deleteUser",  status_code=status.HTTP_201_CREATED)
+async def delete_user(request : DeleteUserRequest):
+    # Check if user exists
+    if not users_collection.find_one({"email": request.email}):
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail=f"User with email {request.email} does not exist."
+            )
+    
+    # Delete user
+    users_collection.delete_one({"email": request.email})
+
+    # delete user from groups
+    groups_collection.update_many(
+        {},
+        {"$pull": {"members": request.email}}
+    )
+
+    # if there are no members in the group, delete it
+    groups_collection.delete_many({"members": []})
+
+    # if there are no members in the subteam, delete it
+    subteams_collection.delete_many({"members": []})
+
+    # delete user from tasks
+    tasks_collection.delete_many({"assigned_to": request.email})
+
+
+@user_router.put("/updateUser", status_code=status.HTTP_201_CREATED)
+async def update_user(request : UpdateUserRequest):
+    # Check if user exists
+    if not users_collection.find_one({"email": request.email}):
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail=f"User with email {request.email} does not exist."
+            )
+
+    # Validate group IDs
+    if not groups_collection.find_one({"_id": ObjectId(request.new_group_id)}):
+        raise HTTPException(status_code=400, detail=f"Group {request.new_group_id} does not exist")
+
+    # Update user
+    users_collection.update_one(
+        {"email": request.email},
+        {"$set": {"name": request.new_name, "groups": [ObjectId(request.new_group_id)]}}
+    )
+    updated_user = users_collection.find_one({"email": request.email})
+
+    return updated_user
 
 
 
