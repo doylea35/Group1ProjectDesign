ca13760614bdfe627b737ace40c5a661d73170f3
lukeorourke <orourklu@tcd.ie>
2025-03-10 18:01:50 +0000

    get files from AWS S3

--- backend/api/routes/files.py
+++ backend/api/routes/files.py
@@ -20,12 +20,31 @@ s3_client = boto3.client(
 
 files_router = APIRouter()
 
+
+@files_router.get("/file/{filename}")
+async def get_presigned_url(filename: str):
+    try:
+
+        presigned_url = s3_client.generate_presigned_url(
+            "get_object",
+            Params={"Bucket": AWS_BUCKET_NAME, "Key": filename},
+            ExpiresIn=3600  # url expires in 1 hour
+        )
+        return {"presigned_url": presigned_url}
+    
+    except Exception as e:
+
+        raise HTTPException(status_code=500, detail=str(e))
+
+
+
 @files_router.post("/upload")
 async def upload_file(file: UploadFile = File(...)):
     """
     uploads a file to AWS S3 and returns the file's public URL
     """
     try:
+
         # upload file
         s3_client.upload_fileobj(file.file, AWS_BUCKET_NAME, file.filename)
 
@@ -35,6 +54,7 @@ async def upload_file(file: UploadFile = File(...)):
     
     except NoCredentialsError:
         raise HTTPException(status_code=500, detail="AWS credentials not found")
+    
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"File upload failed: {str(e)}")
 
