038c7fe13bde64e375edaa5f5c9c1934b596f398
Aina <aina.gomila@estudiantat.upc.edu>
2025-03-27 12:38:45 +0000

    updateUser: deleted cv_skills_extraction functionalities to merge branch fully functioning into main

--- backend/api/request_model/user_request_schema.py
+++ backend/api/request_model/user_request_schema.py
@@ -26,12 +26,3 @@ class UserRegisterRequest(BaseModel):
 class UserLoginRequest(BaseModel):
     email: str
     password: str
-
-class UploadCVRequest(BaseModel):
-    # upload CV request
-    email: str
-    cv: str
-
-class GetSkillsCVRequest(BaseModel):
-    # get user skills from CV
-    email: str
