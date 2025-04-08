6c065210aa9a34ee09f99f544801f5b88c4916d2
Nuoxi Zhang <nzhang@tcd.ie>
2025-04-08 19:24:09 +0100

    Fixed group membership confirmation for unregistered users

--- frontend/src/pages/confirmMembership.jsx
+++ frontend/src/pages/confirmMembership.jsx
@@ -3,43 +3,45 @@ import { useParams, useNavigate } from "react-router-dom";
 import axios from "axios";
 
 const ConfirmGroupMembershipPage = () => {
-    const navigate = useNavigate();
-    const { user_email, group_id } = useParams();
-    const [message, setMessage] = useState("Verifying your email...");
+  const navigate = useNavigate();
+  const { user_email, group_id } = useParams();
+  const [message, setMessage] = useState("Verifying your email...");
 
-    useEffect(() => {
-        if (user_email === 'notRegistered') {
-            setMessage("You've been invited to a GroupGrade Project. Create an account to join.");
-            setTimeout(() => navigate('/'), 3000);
+  useEffect(() => {
+    axios
+      .get(`/api/group/confirmMembership/${user_email}/${group_id}`)
+      .then((response) => {
+        if (response.data.detail.emailNotRegistered) {
+          localStorage.setItem(
+            "confirmGroupMemebershipDetails",
+            JSON.stringify({ group_id, user_email })
+          );
+          setMessage(
+            "You've been invited to a GroupGrade Project. Create an account to join."
+          );
+          setTimeout(() => navigate("/"), 3000);
+        } else {
+          setMessage(
+            "You have succesfully joined the group! Redirecting to the home page..."
+          );
+          setTimeout(() => navigate("/"), 3000); // Redirect to home after a delay
         }
-        else {
-            axios
-                .get(
-                    `/api/group/confirmMembership/${user_email}/${group_id}`
-                )
-                .then((response) => {
-                    setMessage(
-                        "You have succesfully joined the group! Redirecting to the home page..."
-                    );
-                    setTimeout(() => navigate("/"), 3000); // Redirect to home after a delay
-                })
-                .catch((error) => {
-                    setMessage("Failed to join group. The link may be expired or invalid.");
-                    console.error(
-                        "Verification failed:",
-                        error.response ? error.response.data : error.message
-                    );
-                });
-        }
+      })
+      .catch((error) => {
+        setMessage("Failed to join group. Please try again");
+        console.error(
+          "Verification failed:",
+          error.response ? error.response.data : error.message
+        );
+      });
+  }, [navigate]);
 
-    }, [navigate]);
-
-    return (
-        <div style={{ padding: "20px", textAlign: "center" }}>
-            <h1>Group Membership Confirmation</h1>
-            <p>{message}</p>
-        </div>
-    );
+  return (
+    <div style={{ padding: "20px", textAlign: "center" }}>
+      <h1>Group Membership Confirmation</h1>
+      <p>{message}</p>
+    </div>
+  );
 };
 
 export default ConfirmGroupMembershipPage;
